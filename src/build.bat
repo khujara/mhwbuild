2ed8c8e2a9ad2cad732d62254e2622cc1f5abd68
khujara <florianflv@gmail.com>
2018-03-03 17:13:16 +0100

    build file

--- src/build.bat
+++ src/build.bat
@@ -10,8 +10,6 @@ set DebugPDB= -Od -Zi -Gm
 set DebugOBJ= -Od -Z7
 set ReleaseOBJ= -O2 -Zi -Zo -Gm-
 
-rem set KHLIB="D:\Dropbox\work\khlib"
-
 rem set Current= %DebugOBJ% -DKH_DEBUG
 set Current= %Release%
 rem set Current= %ReleaseOBJ% -DKH_DEBUG
@@ -19,12 +17,11 @@ rem set Current= %ReleaseOBJ% -DKH_DEBUG
 set STBWarnings= -wd4244 -wd4701 -wd4703
 set NoErrors= -wd4505 -wd4201 -wd4100 -wd4189 -wd4127 %STBWarnings%
 set CompilerOptions= %Current% -MTd -nologo -Gm- -GR- -EHa- -EHsc -Oi -fp:fast -fp:except- -WX -W4 -FC %NoErrors%
-set LinkerOptions= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib Comdlg32.lib -DEBUG 
-set INC=-I%KHLIB%
+set LinkerOptions= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib
 
 IF NOT EXIST .\..\build\ mkdir .\..\build
 cd .\..\build
 
-cl -DWIN32 %CompilerOptions% %INC% -D_CRT_SECURE_NO_WARNINGS /Femhwbuild.exe ..\src\main.cpp /link %LinkerOptions%
+cl -DWIN32 %CompilerOptions% -D_CRT_SECURE_NO_WARNINGS /Femhwbuild.exe ..\src\main.cpp /link %LinkerOptions%
 
 cd .\..\src
